168137700d5cfc2a1c08683a27effe0aafe8f24f
Kutlák Tamás <kutlak.tamas@econsult.hu>
2023-11-06 09:06:12 +0100

    test save

--- src/app/shared/functions/classToInterface.ts
+++ src/app/shared/functions/classToInterface.ts
@@ -4,15 +4,29 @@ import {GroupNode} from "../classes/formNodes/GroupNode";
 import {BaseNode} from "../classes/formNodes/BaseNode";
 
 export function classToInterface(node: FormNode|BaseNode): Form{
+  // const form: Form = {
+  //   name: node.label,
+  //   code: node.code,
+  //   id: node.getProperty('ID') !== undefined ? +<string>node.getProperty('ID') : null,
+  //   creationDate: <string>node.getProperty('Creation Date'),
+  //   isValid: <boolean><unknown>node.getProperty('Is Valid'),
+  //   lastModificationDate: <string>node.getProperty('Latest Modification Date'),
+  //   groups: [],
+  //   propertyList: []
+  // };
+
   const form: Form = {
     name: node.label,
     code: node.code,
-    id: node.getProperty('ID') !== undefined ? +<string>node.getProperty('ID') : null,
-    creationDate: <string>node.getProperty('Creation Date'),
-    isValid: <boolean><unknown>node.getProperty('Is Valid'),
-    lastModificationDate: <string>node.getProperty('Latest Modification Date'),
+    id: 0,
+    creationDate: new Date().toISOString(),
+    isValid: true,
+    lastModificationDate: new Date().toISOString(),
     groups: [],
     propertyList: []
   };
+
+  console.log(form)
+
   return form;
 }
